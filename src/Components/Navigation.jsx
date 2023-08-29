cce22a1c8c3a449655420284516e094736991e5e
fauzarizky <fauzasmg@gmail.com>
2023-08-29 08:46:58 +0700

    Change nav color

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,12 +1,21 @@
-import { Box, Input, Button, Text } from '@chakra-ui/react'
+import { Box, Input, Button, Text, ButtonGroup } from "@chakra-ui/react";
+import "../index.css";
 
 export default function Navigation() {
   return (
-    <Box display={'flex'} flexDirection={'row'}>
-        <Text>myTix</Text>
-        <Input size={'md'} placeholder='Search an event...'/>
-        <Button bg={'#FFD700'} color={'#917800'}>LOGIN</Button>
-        <Button bg={'#F7F7F7'} color={'#333333'}>REGISTER</Button>
+    <Box display={"flex"} justifyContent={"space-evenly"} shadow={"md"} p={"10px"}>
+      <Text fontSize={"2xl"} fontWeight={"bold"} bg={'#383280'} px={'15px'} color={'white'} borderRadius={'md'}>
+        myTix
+      </Text>
+      <Input size={"md"} placeholder="Search an event..." w={"md"} shadow={"sm"} />
+      <ButtonGroup>
+        <Button bg={"#3E60C1"} color={"white"} className="btn-nav">
+          LOGIN
+        </Button>
+        <Button bg={"#F7F7F7"} color={"#2e4583"}>
+          REGISTER
+        </Button>
+      </ButtonGroup>
     </Box>
-  )
+  );
 }
