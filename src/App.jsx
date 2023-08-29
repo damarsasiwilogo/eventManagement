b2e12fbe133d456e6fe39479f2560d434ed0c29a
damarsasiwilogo <damarsasiwilogo@gmail.com>
2023-08-29 14:37:49 +0700

    adding regist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Regist from "./pages/Regist";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="./pages/regist" element={<Regist/>}></Route>
       </Routes>
     </>
   );
