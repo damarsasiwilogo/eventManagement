e6be8a7bcab75d1434e394a855ff50036f009e70
anggaratriputra <anggaratriputra@gmail.com>
2023-08-31 08:42:08 +0700

    add Transaction and change event.json

---
+++ src/Components/TransactionStep1.jsx
@@ -0,0 +1,116 @@
+import { Box, Button, Divider, Flex, Image, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+function TransactionStep1() {
+    const [quantityFestivalA, setQuantityFestivalA] = useState(0);
+    const [quantityFestivalB, setQuantityFestivalB] = useState(0);
+    const ticketPriceFestivalA = 950000;
+    const ticketPriceFestivalB = 850000;
+
+    const totalFestivalA = quantityFestivalA * ticketPriceFestivalA;
+    const totalFestivalB = quantityFestivalB * ticketPriceFestivalB;
+    const grandTotal = totalFestivalA + totalFestivalB;
+
+    return (
+        <>
+            
+            <Box display={"flex"} flexDirection="column" bgColor="#EDEDED" ml={40} mr={40} mt={2} borderTopRadius={10}>
+                <Text display="flex" justifyContent="flex-start" ml={4} mt={4}>
+                    PILIH KATEGORI
+                </Text>
+                <Box mx={4} mt={3}>
+                    <Divider borderColor="blackAlpha.300" />
+                </Box>
+                <Image src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/seating_chart/20230716041429.png" mx={20} mt={4} />
+                <Flex mx={4}>
+                    <Table variant="simple" colorScheme="blackAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th>Jenis Kategori</Th>
+                                <Th textAlign="center">Harga per tiket</Th>
+                                <Th textAlign="center">Kuantitas</Th>
+                                <Th textAlign="center" width="200px">Jumlah</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>
+                                    <Text fontSize="xs" colorScheme="blackAlpha">
+                                        FESTIVAL A <br />
+                                        Standing <br />
+                                        Harga tiket belum termasuk biaya platform dan pajak daerah
+                                    </Text>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Text fontSize="xs" colorScheme="blackAlpha">
+                                        Rp. 950.000
+                                    </Text>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Flex gap={1} alignItems="center" justifyContent="center">
+                                        <Button bg={"#331F69"} color="white" size="xs" onClick={() => setQuantityFestivalA(Math.max(0, quantityFestivalA - 1))}>
+                                            -
+                                        </Button>
+                                        <Text fontSize="xs">{quantityFestivalA}</Text>
+                                        <Button bg={"#331F69"} color="white" size="xs" onClick={() => setQuantityFestivalA(quantityFestivalA + 1)}>
+                                            +
+                                        </Button>
+                                    </Flex>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Text fontSize="xs"> Rp. {totalFestivalA.toLocaleString()} </Text>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td>
+                                    <Text fontSize="xs" colorScheme="blackAlpha">
+                                        FESTIVAL B <br />
+                                        Standing <br />
+                                        Harga tiket belum termasuk biaya platform dan pajak daerah
+                                    </Text>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Text fontSize="xs" colorScheme="blackAlpha">
+                                        Rp. 850.000
+                                    </Text>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Flex gap={1} alignItems="center" justifyContent="center">
+                                        <Button bg={"#331F69"} color="white" size="xs" onClick={() => setQuantityFestivalB(Math.max(0, quantityFestivalB - 1))}>
+                                            -
+                                        </Button>
+                                        <Text fontSize="xs"> {quantityFestivalB} </Text>
+                                        <Button bg={"#331F69"} color="white" size="xs" onClick={() => setQuantityFestivalB(quantityFestivalB + 1)}>
+                                            +
+                                        </Button>
+                                    </Flex>
+                                </Td>
+                                <Td textAlign="center">
+                                    <Text fontSize="xs"> Rp. {totalFestivalB.toLocaleString()} </Text>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td colSpan={3} textAlign="center">
+                                    Total
+                                </Td>
+                                <Td textAlign="center">
+                                    <Text fontWeight="bold">
+                                        Rp. {grandTotal.toLocaleString()}
+                                    </Text>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Flex>
+            </Box>
+            <Box display={"flex"} bgColor="#EDEDED" justifyContent="flex-end" h={"10vh"} ml={40} mr={40} mb={5} borderBottomRadius={10}>
+                <Button colorScheme="whatsapp" size="sm" mr={20} mt={5}>
+                   Checkout
+                </Button>
+            </Box>
+
+        </>
+    )
+}
+
+export default TransactionStep1;
--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -1,7 +1,78 @@
-import { Box, Divider, Flex, Image, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react"
-import React from "react"
+import { Box, Button, Divider, Flex, Image, Table, Tbody, Td, Text, Th, Thead, Tr, Progress } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import TransactionStep1 from "../Components/TransactionStep1";
+import TransactionStep2 from "../Components/TransactionStep2";
+import TransactionStep3 from "../Components/TransactionStep3";
+import TransactionStep4 from "../Components/TransactionStep4";
+
 
 function Transaction() {
+    const [currentStep, setCurrentStep] = useState(1);
+    const [remainingTime, setRemainingTime] = useState(15 * 60); // Waktu dalam detik (15 menit)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (remainingTime > 0) {
+                setRemainingTime(remainingTime - 1);
+            } else {
+                clearInterval(interval);
+                // Waktu habis, lakukan pengaturan ulang atau merender ulang komponen
+                // Misalnya: setCurrentStep(1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [remainingTime]);
+
+    const handleNext = () => {
+        if (currentStep < 4) {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const renderStep = () => {
+        switch (currentStep) {
+            case 1:
+                return <TransactionStep1 onNext={handleNext} />;
+            case 2:
+                return <TransactionStep2 onNext={handleNext} />;
+            case 3:
+                return <TransactionStep3 onNext={handleNext} />;
+            case 4:
+                return <TransactionStep4 />;
+            default:
+                return null;
+        }
+    };
+
+
+    const calculateProgress = () => {
+        switch (currentStep) {
+            case 1:
+                return 25;
+            case 2:
+                return 50;
+            case 3:
+                return 75;
+            case 4:
+                return 100;
+            default:
+                return 0;
+        }
+    };
+
+    const progressBarWidth = `${calculateProgress()}%`;
+
+    const stepTexts = [
+        "Pilih Kategori",
+        "Informasi Personal",
+        "Konfirmasi",
+        "Bayar"
+    ];
+
+
+
     return (
         <>
             <Box display={"flex"} justifyContent="flex-start" bg={"#331F69"} alignItems={"center"} h={"10vh"}>
@@ -11,52 +82,34 @@ function Transaction() {
             </Box>
             <Box display={"flex"} justifyContent="center" alignItems={"center"} h={"30vh"} ml={40} mr={40} mt={5} bgImage="url('https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/tgroupbanner/20230716041255.png')" bgRepeat="no-repeat" bgSize="cover" bgPos="center" borderRadius={10}>
             </Box>
-            <Box display={"flex"} justifyContent="center" bgColor="#EDEDED" alignItems={"center"} h={"10vh"} ml={40} mr={40} mt={2} borderRadius={10}>
-                WAKTU TERSISA
-            </Box>
-            <Box display={"flex"} flexDirection="column" bgColor="#EDEDED" ml={40} mr={40} mt={2} mb={4} borderRadius={10}>
-                <Text display="flex" justifyContent="flex-start" ml={4} mt={4}>
-                    PILIH KATEGORI
+            <Box display={"flex"} flexDirection="column" justifyContent="center" bgColor="#EDEDED" alignItems={"center"} h={"10vh"} ml={40} mr={40} mt={2} borderRadius={10}>
+                <Text fontSize={"md"} fontWeight={"bold"} mt={2}>
+                    WAKTU TERSISA
                 </Text>
-                <Box mx={4} mt={3}>
-                    <Divider borderColor="blackAlpha.300" />
-                </Box>
-                <Image src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/seating_chart/20230716041429.png"
-                    mx={20} mt={4}></Image>
-                <Flex mx={4}>
-                    <Table variant="simple" colorScheme="blackAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th>Jenis Kategori</Th>
-                                <Th textAlign="center">Harga per tiket</Th>
-                                <Th textAlign="center">Kuantitas</Th>
-                                <Th textAlign="center">Jumlah</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td>
-                                    <Text fontSize="xs" colorScheme="blackAlpha">
-                                        FESTIVAL A <br />
-                                        Standing <br />
-                                        Harga tiket belum termasuk biaya platform dan pajak daerah
-                                    </Text>
-                                </Td>
-                                <Td textAlign="center">
-                                    <Text fontSize="xs" colorScheme="blackAlpha">
-                                        Rp. 950.000
-                                    </Text>
-                                </Td>
-                                <Td textAlign="center"> counter </Td>
-                                <Td textAlign="center"> jumlah </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                </Flex>
-            </Box >
+                <Text fontSize={"4xl"} fontWeight={"bold"} mb={2} color={"red"}>
+                    {Math.floor(remainingTime / 60)} : {(remainingTime % 60).toString().padStart(2, "0")}
+                </Text>
+            </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center" h="5vh" ml={60} mr={60}>
+                {stepTexts.map((text, index) => (
+                    <Text
+                        key={index}
+                        fontSize="lg"
+                        fontWeight={index === currentStep - 1 ? "bold" : "normal"}
+                    >
+                        {text}
+                    </Text>
+                ))}
+            </Box>
+            {/* Progress Bar */}
+            <Box display="flex" flexDir="column" justifyContent="center" ml={40} mr={40} borderRadius={10}>
+                <Progress mx="4px" value={calculateProgress()} size="md" colorScheme="facebook" borderRadius={10} />
+            </Box>
+
+
+
+            {renderStep()}
         </>
     )
 }
-
-
-export default Transaction
+export default Transaction;
