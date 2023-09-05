72899bb2ffed2388cdf9dad4a40b1f9d5710bef8
damarsasiwilogo <damarsasiwilogo@gmail.com>
2023-09-05 20:31:52 +0700

    lebih baru lagi

--- src/slices/userSlices.js
+++ src/slices/userSlices.js
@@ -1,44 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	//Registration
-	users: [],
-	totalData: 0,
-	isLoaded: false,
-	//Login
-	isLoggedIn: window.localStorage.getItem("isLoggedIn") === "true",
-	profile: window.localStorage.getItem("profile")
-		? JSON.parse(window.localStorage.getItem("profile"))
-		: {},
+  //Registration
+  users: [],
+  totalData: 0,
+  isLoaded: false,
+  //Login
+  isLoggedIn: window.localStorage.getItem("isLoggedIn") === "true",
+  profile: window.localStorage.getItem("profile")
+    ? JSON.parse(window.localStorage.getItem("profile"))
+    : {},
 };
 
 const usersSlice = createSlice({
-	name: "users",
-	initialState,
-	reducers: {
-		//Registration
-		setInitialData(state, action) {
-			state.users = action.payload;
-			state.totalData = action.payload.length.length;
-			state.isLoaded = true;
-		},
-		add(state, action) {
-			state.users.push(action.payload);
-			state.totalData++;
-		},
-		login(state, action) {
-			state.isLoggedIn = true;
-			state.profile = action.payload;
-			window.localStorage.setItem("isLoggedIn", "true");
-			window.localStorage.setItem("profile", JSON.stringify(action.payload));
-		},
-		logout(state) {
-			state.isLoggedIn = false;
-			state.profile = {};
-			window.localStorage.setItem("isLoggedIn", false);
-			window.localStorage.setItem("profile", JSON.stringify({}));
-		},
-	},
+  name: "users",
+  initialState,
+  reducers: {
+    //Registration
+    setInitialData(state, action) {
+      state.users = action.payload;
+      state.totalData = action.payload.length.length;
+      state.isLoaded = true;
+    },
+    add(state, action) {
+      state.users.push(action.payload);
+      state.totalData++;
+    },
+    login(state, action) {
+      state.isLoggedIn = true;
+      state.profile = action.payload;
+      window.localStorage.setItem("isLoggedIn", "true");
+      window.localStorage.setItem("profile", JSON.stringify(action.payload));
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+      state.profile = {};
+      window.localStorage.setItem("isLoggedIn", false);
+      window.localStorage.setItem("profile", JSON.stringify({}));
+    },
+  },
 });
 
 export const { setInitialData, add, login, logout } = usersSlice.actions;
