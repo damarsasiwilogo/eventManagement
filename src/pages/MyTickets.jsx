a5d5248226feddbe622ba0c996e5ecddc0a1ecf2
anggaratriputra <anggaratriputra@gmail.com>
2023-09-16 14:28:35 +0700

    fixing minor bug like false scalling

--- src/pages/MyTickets.jsx
+++ src/pages/MyTickets.jsx
@@ -140,9 +140,9 @@ function MyTickets() {
                 <Text fontSize={{base:"12px", lg: "20px"}}textAlign={"center"}> {calculateRemainingTime(data.timestamp) !== "Payment Canceled" ? data.status : "Ticket cancelled due to payment timeout"} </Text>
                 <Text fontSize={{base:"12px", lg: "20px"}}> {data.eventname} </Text>
               </Flex>
-              <Flex h={{base: "536px"}} bgColor={calculateRemainingTime(data.timestamp) === "Payment Canceled" ? "grey" : "#331F69"} alignItems="center" boxShadow="2xl" direction={{ base: "column", md: "column", lg: "row" }}>
+              <Flex h={{base: "536px"}} bgColor={calculateRemainingTime(data.timestamp) === "Payment Canceled" ? "grey" : "#331F69"} alignItems={{lg: "center"}} boxShadow="2xl" direction={{ base: "column", md: "column", lg: "row" }}>
                 <Flex direction={"row"} gap={2} p={{ base: "2px", md: "4px", lg: "10px" }} m={{base:"1" ,lg: "4"}}>
-                  <Flex direction={"column"} justifyContent={{lg: "center"}}>
+                  <Flex direction={"column"} ml={{base: "15px"}} justifyContent={{lg: "center"}}>
                     <Text fontWeight={"bold"} fontSize={{ base: "10px", md: "16px", lg: "18px" }} color="white">
                       Purchase Date
                     </Text>
